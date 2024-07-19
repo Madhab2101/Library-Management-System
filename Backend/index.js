8df4fa592596d53030b4f95f51a24bb767a3e19c
Madhab2101 <153718753+Madhab2101@users.noreply.github.com>
2024-07-19 15:44:22 +0530

    Backend setup done

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 const PORT=process.env.PORT || 4000;
 
+// connect to mongodb
+const URI=process.env.MongoDBURI;
+try {
+    mongoose.connect(URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    });
+    console.log("Connected to Mongodb");
+    
+} catch (error) {
+    console.log("Error",error);
+    
+    
+}
 
-app.get('/', (req, res) => {
-  res.send('Hel World!')
-})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
